9ea9de04d8d0e9ed06e2b0be0f7dda7ba64bac5a
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-02-02 21:58:43 -0500

    first commit

---
+++ StarLeague_SF.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    FILE *junk;
+    junk = freopen(".in", "r", stdin);
+    junk = freopen(".out", "w", stdout);
+    
+    return 0;
+}
