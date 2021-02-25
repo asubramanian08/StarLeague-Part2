097948cc48dba3e6a41db5fb42adc13e7763eb1e
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-02-25 07:43:08 -0500

    q3 week 6 finished

--- W1-W6_DP/W6-ShortestPath/Problems-6/q3.cpp
+++ W1-W6_DP/W6-ShortestPath/Problems-6/q3.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
+//getting things ready
 #define BIG_NUM 1000000
+typedef pair<int, int> pii;
 struct pie
 {
     int bVal;
@@ -10,13 +13,14 @@ struct pie
     int orgLoc;
 };
 
+//compares
 int bCompare(const void *p1, const void *p2)
 {
-    return ((pie *)p1)->bVal - ((pie *)p2)->bVal;
+    return ((pie *)p1)->eVal - ((pie *)p2)->eVal;
 }
 int eCompare(const void *p1, const void *p2)
 {
-    return ((pie *)p1)->eVal - ((pie *)p2)->eVal;
+    return ((pie *)p1)->bVal - ((pie *)p2)->bVal;
 }
 int orgCompare(const void *p1, const void *p2)
 {
@@ -30,27 +34,29 @@ vector<int> *bConns;
 vector<int> *eConns;
 int *bCost;
 int *eCost;
-void findB_Cost(int node, int parent);
-void findE_Cost(int node, int parent);
+queue<int> bQ;
+queue<int> eQ;
 
-void findB_Cost(int node, int parent)
+//b&e BFS funcs
+void bFind(int source)
 {
-    if (bCost[node] != BIG_NUM)
-        return;
-    for (int i = 0; i < bConns[node].size(); i++)
-        if (bConns[node][i] != parent)
-            findB_Cost(bConns[node][i], node);
+    for (int i = 0; i < bConns[source].size(); i++)
+        if (eCost[bConns[source][i]] > bCost[source] + 1)
+        {
+            eCost[bConns[source][i]] = bCost[source] + 1;
+            eQ.push(bConns[source][i]);
+        }
+}
+void eFind(int source)
+{
+    for (int i = 0; i < eConns[source].size(); i++)
+        if (bCost[eConns[source][i]] > eCost[source] + 1)
+        {
+            bCost[eConns[source][i]] = eCost[source] + 1;
+            bQ.push(eConns[source][i]);
+        }
+}
 
-    if (bPies[node].eVal == 0)
-    {
-        bCost[node] = 1;
-        return;
-    }
-    bCost
-}
-void findE_Cost(int node, int parent)
-{
-}
 int main(void)
 {
     /*FILE *junk;
@@ -76,43 +82,90 @@ int main(void)
     }
     qsort(ePies, numPies, sizeof(pie), eCompare);
 
-    //find graph
+    //find graph (reverse direction)
     bConns = new vector<int>[numPies];
-    for (int i = 0; i < numPies; i++)
-    {
-        int low = 0;
-        int high = numPies;
-        int goal = bPies[i].eVal - range;
-        while (high - low > 1)
-        {
-            if (ePies[(high + low) / 2].eVal < goal)
-                low = (high + low) / 2;
-            else
-                high = (high + low) / 2;
-        }
-        for (int j = high; (j < numPies) && (j < bPies[i].eVal + range); j++)
-            bConns[i].push_back(ePies[j].orgLoc);
-    }
     eConns = new vector<int>[numPies];
     for (int i = 0; i < numPies; i++)
     {
         int low = 0;
         int high = numPies;
-        int goal = ePies[i].bVal - range;
+        int goal = bPies[i].bVal - range;
+        if (ePies[low].bVal >= goal)
+            high = low;
         while (high - low > 1)
         {
-            if (bPies[(high + low) / 2].bVal < goal)
+            if (ePies[(high + low) / 2].bVal < goal)
                 low = (high + low) / 2;
             else
                 high = (high + low) / 2;
         }
-        for (int j = high; (j < numPies) && (j < ePies[i].bVal + range); j++)
-            bConns[i].push_back(bPies[j].orgLoc);
+        for (int j = high; (j < numPies) && (ePies[j].bVal <= bPies[i].bVal); j++)
+            bConns[bPies[i].orgLoc].push_back(ePies[j].orgLoc);
+    }
+    for (int i = 0; i < numPies; i++)
+    {
+        int low = 0;
+        int high = numPies;
+        int goal = ePies[i].eVal - range;
+        if (bPies[low].eVal >= goal)
+            high = low;
+        while (high - low > 1)
+        {
+            if (bPies[(high + low) / 2].eVal < goal)
+                low = (high + low) / 2;
+            else
+                high = (high + low) / 2;
+        }
+        for (int j = high; (j < numPies) && (bPies[j].eVal <= ePies[i].eVal); j++)
+            eConns[ePies[i].orgLoc].push_back(bPies[j].orgLoc);
     }
     qsort(bPies, numPies, sizeof(pie), orgCompare);
     qsort(ePies, numPies, sizeof(pie), orgCompare);
 
+    //set costs and start queue
     bCost = new int[numPies];
     eCost = new int[numPies];
+    for (int i = 0; i < numPies; i++)
+        if (bPies[i].eVal == 0)
+        {
+            bCost[i] = 1;
+            bQ.push(i);
+        }
+        else
+            bCost[i] = BIG_NUM;
+    for (int i = 0; i < numPies; i++)
+        if (ePies[i].bVal == 0)
+        {
+            eCost[i] = 1;
+            eQ.push(i);
+        }
+        else
+            eCost[i] = BIG_NUM;
+
+    //compute costs
+    while (!bQ.empty() || !eQ.empty())
+    {
+        if (eQ.empty() || (!bQ.empty() && bCost[bQ.front()] < eCost[eQ.front()]))
+        {
+            bFind(bQ.front());
+            bQ.pop();
+        }
+        else
+        {
+            eFind(eQ.front());
+            eQ.pop();
+        }
+    }
+
+    //print out
+    for (int i = 0; i < numPies; i++)
+    {
+        if (bCost[i] != BIG_NUM)
+            cout << bCost[i] << endl;
+        else
+            cout << "-1" << endl;
+    }
+
+    //return
     return 0;
 }
