87230952fa98c666bb4d8d807e9ee025ff4eddd7
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-02-27 21:32:08 -0500

    week 7 q4

--- W7-W8_Queries/W7-RangeQueries/Problems-7/q4.cpp
+++ W7-W8_Queries/W7-RangeQueries/Problems-7/q4.cpp
@@ -1,38 +1,85 @@
 #include <iostream>
 using namespace std;
 
-//bit tree stuff
-int *bTree;
-int elements;
-void initNoArr(int elem)
+//BIT tree class
+class BIT
+{
+private:
+    int *bTree;
+    int elements;
+
+public:
+    BIT(int elem);
+    BIT(int elem, int *initialize, int numInit);
+    ~BIT() { delete[] bTree; }
+    int query(int node);
+    void updt(int node, int cng);
+};
+BIT::BIT(int elem)
 {
     elements = elem;
-    bTree = new int[elem];
+    bTree = new int[elem + 1];
+    for (int i = 1; i <= elem; i++)
+        bTree[i] = 0;
 }
-void initArr(int elem, int *initialize, int numInit)
+BIT::BIT(int elem, int *initialize, int numInit)
 {
     elements = elem;
-    bTree = new int[elem];
+    bTree = new int[elem + 1];
+    for (int i = 1; i <= elem; i++)
+        bTree[i] = 0;
     for (int i = 0; i < numInit; i++)
         updt(i, initialize[i]);
 }
-int query(int node)
+int BIT::query(int node)
 {
     int sum = 0;
-    for (int i = node - 1; i >= 0; i -= (i & -i))
+    for (int i = node + 1; i > 0; i -= (i & -i))
         sum += bTree[i];
     return sum;
 }
-void updt(int node, int val)
+void BIT::updt(int node, int cng)
 {
-    for (int i = node - 1; i < elements; i += (i & -i))
-        bTree[i] += val;
+    for (int i = node + 1; i <= elements; i += (i & -i))
+        bTree[i] += cng;
 }
 
 int main(void)
 {
-    FILE *junk;
+    /*FILE *junk;
     junk = freopen("circlecross.in", "r", stdin);
-    junk = freopen("circlecross.out", "w", stdout);
+    junk = freopen("circlecross.out", "w", stdout);*/
+    //read
+    int cows;
+    cin >> cows;
+    int numCross = cows << 1;
+    int *crossPoint = new int[numCross];
+    for (int i = 0; i < numCross; i++)
+        cin >> crossPoint[i];
+    for (int i = 0; i < numCross; i++)
+        crossPoint[i]--;
+
+    //initialize
+    int *start = new int[cows]; //-1 = not started
+    for (int i = 0; i < cows; i++)
+        start[i] = -1;
+    BIT circle(numCross);
+
+    //compute the pairs
+    int pairs = 0;
+    for (int i = 0; i < numCross; i++)
+        if (start[crossPoint[i]] == -1)
+        {
+            circle.updt(i, 1);
+            start[crossPoint[i]] = i;
+        }
+        else
+        {
+            circle.updt(start[crossPoint[i]], -1);
+            pairs += circle.query(i) - circle.query(start[crossPoint[i]]);
+        }
+
+    //print and return
+    cout << pairs;
     return 0;
 }
